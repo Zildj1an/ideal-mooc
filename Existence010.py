199b26460b72572579c702ad74a2494fb937b8aa
Tobias Klauser <tklauser@distanz.ch>
2014-10-15 12:40:10 +0200

    Initial import

---
+++ Existence010.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Number of time steps to perform the experiment for
+N_CYCLES = 11
+
+def pickOtherExperiment(currentExperiemnt):
+    for e in ['e1', 'e2']:
+        if e != currentExperiemnt:
+            return e
+
+# anticipate experiment result based on stored interactions, might return None
+def anticipate(experiment):
+    return interactions.get(experiment)
+
+# store an interaction tuple (experiment, result)
+def recordInteraction(experiment, result):
+    interactions[experiment] = result
+
+experiment = 'e1'
+mood = None
+selfSatisfiedDuration = 0
+
+# stored interactions (experiment, result)
+interactions = {}
+
+for cycle in range(N_CYCLES):
+    if mood == 'BORED':
+        selfSatisfiedDuration = 0
+        experiment = pickOtherExperiment(experiment)
+
+    anticipatedResult = anticipate(experiment)
+
+    # environment
+    if experiment == 'e1':
+        result = 'r1'
+    else:
+        result = 'r2'
+
+    recordInteraction(experiment, result)
+
+    if result == anticipatedResult:
+        mood = 'SELF-SATISFIED'
+        selfSatisfiedDuration += 1
+    else:
+        mood = 'FRUSTRATED'
+        selfSatisfiedDuration = 0
+
+    if selfSatisfiedDuration > 3:
+        mood = 'BORED'
+
+    print(cycle, experiment, result, mood)
---
+++ README.md
@@ -0,0 +1,17 @@
+IDEAL MOOC Programming Exercises
+================================
+
+Programming exercises for the [IDEAL MOOC](http://liris.cnrs.fr/ideal/mooc/), rewritten in Python.
+
+References
+==========
+
+* [Syllabus](http://liris.cnrs.fr/ideal/mooc/syllabus.html)
+* [Lessons](http://liris.cnrs.fr/ideal/mooc/lesson.php)
+* [Original Exercises](https://code.google.com/p/developmental-learning-tutorial/) (in Java)
+* [Google+ Community](https://plus.google.com/u/0/communities/109445848302721599408)
+
+Other People
+============
+
+* martinxyz: https://github.com/martinxyz/ideal-mooc
